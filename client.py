1fff9ca9e849abd169f6e0533d2ec20966517f59
Jaysen Gomez <116235937+gomezjesse27@users.noreply.github.com>
2023-06-15 17:52:44 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,17 +1,31 @@
-from socket import *
+import socket
+import sys
 
-server_name = 'localhost'
-server_port = 800
+def send_http_request(server_host, server_port, filename):
+    # create a socket / establish connection
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect((server_name, server_port))
 
-sentence = input('Input lowercase sentence: ')
-request = f"GET /{sentence} HTTP/1.1\r\nHost: {server_name}\r\n\r\n"
-client_socket.send(request.encode())
+    clientsocket.connect((server_host, server_port))
 
-response = client_socket.recv(4096)
-print('Server response:')
-print(response.decode())
+    # HTTP request
+    request = f"GET /{filename} HTTP/1.1\r\nHost: {server_host}\r\n\r\n"
+    clientsocket.send(request.encode())
 
-client_socket.close()
+    # print the servers response
+    modifiedSentence = clientsocket.recv(1024).decode()
+
+
+    print("From server:\n", modifiedSentence)
+
+    # closing the socket
+    clientsocket.close()
+
+# instead of an input line like the slide examples we use this to take aruments like the lab outlines
+
+server_host = sys.argv[1]
+server_port = int(sys.argv[2])
+filename = sys.argv[3]
+
+# HTTP request
+send_http_request(server_host, server_port, filename)
